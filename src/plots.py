c3a800341ac5ca9f4ebaee20e25d753d7b4547e4
Ramya Ramakrishnan <ramyaram@mit.edu>
2017-02-24 15:03:44 -0500

    Plotting graph for one task, not source and target

--- src/plots.py
+++ src/plots.py
@@ -8,13 +8,7 @@ import math
 import numpy as np
 from scipy import interpolate
 
-plt.gcf().subplots_adjust(bottom=0.15)
-
-num_figs = 2
-subplot_nums = [111] #[211,212]
 colors = ['b','r','g']
-titles = ['All Reward','Avg Reward']
-
 cnt=0
 f = file(sys.argv[1]+'/'+sys.argv[2]).read().strip().split('\n')
 for line in f:
@@ -25,27 +19,17 @@ for line in f:
 #    x_int = np.linspace(x[0], x[-1], 100)
 #    tck = interpolate.splrep(x, y, k = 3, s = 1)
 #    y_int = interpolate.splev(x_int, tck, der = 0)
-    if 'SOURCE' in label: #source task learning
-        plt.figure(0)
-    else: #target task learning
-        plt.figure(1)
-    plt.subplot(subplot_nums[0])
+    plt.figure(0)
     plt.plot(x, y, label = label, linestyle = '-', linewidth = 0.75, color=colors[cnt])
 #    plt.subplot(subplot_nums[1])
 #    plt.plot(x_int, y_int, label = label, linestyle = '-', linewidth = 0.75, color=colors[cnt])
     cnt+=1
 
-for fig in range(0,num_figs):
-    for num in range(len(subplot_nums)):
-        plt.figure(fig)
-        plt.subplot(subplot_nums[num])
-        plt.subplots_adjust(hspace=1)
-        plt.title(titles[num])
-        plt.legend(loc=4, fontsize = 'x-small')
-        plt.ylabel('Average Accumulated\n'+titles[num])
-        plt.xlabel('Number of Episodes')
+plt.figure(0)
+plt.title('Learning Curve')
+plt.legend(loc=4, fontsize = 'x-small')
+plt.ylabel('Average Accumulated\nReward')
+plt.xlabel('Number of Episodes')
 
 plt.figure(0)
-plt.savefig(sys.argv[1]+'/source_task.pdf')
-plt.figure(1)
-plt.savefig(sys.argv[1]+'/target_task.pdf')
+plt.savefig(sys.argv[1]+'/plot.pdf')

5e6208b70e5b6d4ec292b358791c2fbc150a751d
Ramya Ramakrishnan <ramyaram@mit.edu>
2017-04-28 15:23:47 -0400

    Running games one-by-one automatically through user input

---
+++ src/run_all.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# This program takes 1 argument, a number between 1 and 4 that specifies the ordering of the test games.
+# For a pair of participants, you can either choose 1 and 2 OR 3 and 4, not any other pair because participants can never play the same testing game at the same time.
+
+order="$1"
+
+declare -a arr1=("X0" "Y0" "Z0" "L0" "V0" "U0" "Q0")
+declare -a arr2=("X0" "Y0" "Z0" "V0" "L0" "Q0" "U0")
+declare -a arr3=("X0" "Y0" "Z0" "V0" "L0" "U0" "Q0")
+declare -a arr4=("X0" "Y0" "Z0" "L0" "V0" "Q0" "U0")
+
+arrStr="arr$order"
+tmp="$arrStr[@]"
+arrTmp=${!tmp}
+arr=( $arrTmp )
+
+i=0
+echo "Playing game $i"
+./run.sh play -g "${arr[i]}"
+
+while [ "$i" -lt "${#arr[@]}" ]; do
+	while true; do
+		read -p "Are you ready to continue?" y
+		case $y in
+			[Yy]* ) break;;
+			* ) echo "Please answer 'y' when ready.";;
+		esac
+	done
+	i=$((i+1)) 
+	echo "Playing game $i"
+	./run.sh play -g "${arr[i]}"
+done

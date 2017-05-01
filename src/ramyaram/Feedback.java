b6aca2c71560985cab91327f76846a82a2df795e
Matthew Beveridge <mattbev@mit.edu>
2017-05-01 17:45:27 -0400

    No splash screen rd. 2

--- src/ramyaram/Feedback.java
+++ src/ramyaram/Feedback.java
@@ -20,10 +20,10 @@ public class Feedback {
     public static void addSound(char sign) throws IOException{
         String loc = "";
         if (sign == '+'){
-            loc = "media/good.m4a";
+            loc = "media/good.au";
         }
         else if (sign == '-') {
-            loc = "media/bad.m4a";
+            loc = "media/bad.au";
         }
         String soundFile = loc;
         InputStream in = new FileInputStream(soundFile);
@@ -32,7 +32,7 @@ public class Feedback {
     }
     
     public static void addFeedback(double ScoreChange, int gameTick) throws IOException{
-        String loc = "";
+//        String loc = "";
 //        SplashScreen m = new SplashScreen();
 //        m.showSplash(loc);
         if(ScoreChange > 0){
@@ -50,7 +50,7 @@ public class Feedback {
 //            else {
 //                loc = "C:\\Users\\mattj\\OneDrive\\Pictures\\IRG\\plus.jpg";
 //                }
-            loc = "media/+10.jpg";
+//            loc = "media/+10.jpg";
         }
         else if (ScoreChange < 0){
             addSound('-');
@@ -66,7 +66,7 @@ public class Feedback {
 //            else {
 //                loc = "C:\\Users\\mattj\\OneDrive\\Pictures\\IRG\\minus.jpg";
 //           }
-            loc = "media/-10.jpg";
+//            loc = "media/-10.jpg";
         }
         
 //       SplashScreen n = new SplashScreen();
